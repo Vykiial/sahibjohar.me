afdfdd1c63e75be911587255b693aa756a034c8f
Sahib <sahibjohar@gmail.com>
2023-01-16 02:33:30 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LifeTimeline from "./pages/LifeTimeline";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return null
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/Projects' element={<Projects />}></Route>
+          <Route path='/LifeTimeLine' element={<LifeTimeline />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  )
 }
 
 export default App;
